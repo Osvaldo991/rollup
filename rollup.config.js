26e8bd7e07ba7bfe775ac09bd53a511f25eccdca
Osvaldo Sanchez <osvaldozurdo7@gmail.com>
2023-04-24 12:31:12 -0600

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+export default {
+    input: "src/index.js",
+    output: {
+        file: 'dist/bundle.js',
+        format: 'es',
+        sourcemap: true,
+    },
+    plugins: [
+        
+
+    ]
+}
