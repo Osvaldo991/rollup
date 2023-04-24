14fc3fc17291443bbca3a0e38507a5d9d3014353
Osvaldo Sanchez <osvaldozurdo7@gmail.com>
2023-04-24 12:49:05 -0600

    segundo

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,8 @@
+import html from '@rollup/plugin-html';
+import copy from 'rollup-plugin-copy';
+import css from 'rollup-plugin-css-only';
+import image from '@rollup/plugin-image';
+
 export default {
     input: "src/index.js",
     output: {
@@ -6,7 +11,18 @@ export default {
         sourcemap: true,
     },
     plugins: [
-        
-
+        copy({
+            targets: [
+                {
+                    src: "src/assets/**/*",
+                    dest: "dest/asset",
+                }
+            ]
+        }),
+        css({
+            output: 'bundle.css' 
+        }),
+        html(),
+        image(),
     ]
 }
---
+++ web-dev-server.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    port: 3007,
+    nodeRedolve: true,
+    open: true,
+    watch: true,
+    appIndex: 'dist/index.html'
+}
